fbfc216eec590a0f505e05762a807d737124eb66
Carlos Ricardo Schwarz Junior <schwarz.junior@gmail.com>
2014-03-25 21:19:13 -0300

    AgendaTelefonica: Classe 'MensageiroView': Novo metodo 'imprimeLinhaSeparadora'.

--- ListaTelefonica/src/view/MensageiroView.java
+++ ListaTelefonica/src/view/MensageiroView.java
@@ -4,6 +4,8 @@ import static java.lang.System.out;
 
 public class MensageiroView
 {
+	private static final int tamanhoPadraoLinhaSeparadora = 70;
+	
 	public static void imprime(String mensagem)
 	{
 		out.print(mensagem);
@@ -17,5 +19,21 @@ public class MensageiroView
 	public static void imprimeLinha()
 	{
 		out.println();
+	}
+	
+	public static void imprimeLinhaSeparadora()
+	{
+		imprimeLinhaSeparadora(tamanhoPadraoLinhaSeparadora);
+	}
+	
+	public static void imprimeLinhaSeparadora(int tamanho)
+	{
+		String linhaSeparadora = "";
+		for (int i = 0; i < tamanho; i++) {
+			linhaSeparadora += "-";
+		}
+		out.println(linhaSeparadora);
+//		out.println(String.format("%0" + tamanho + "d", 0).replace("0", "-"));
+//		out.println(new String(new char[tamanho]).replace("\0", "-"));
 	}
 }
